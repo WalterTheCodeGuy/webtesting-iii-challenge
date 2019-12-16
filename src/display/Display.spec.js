faa245e75ca8a76154d4939040d5cd6b714549e5
Walter Futch <walter.futch@gmail.com>
2019-12-16 15:03:53 -0500

    Display Test Complete
    Display.spec.js complete and working

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,23 @@
-// Test away!
+import React from 'react';
+import Display from './Display';
+import {render, fireEvent, cleanup} from '@testing-library/react';
+import renderer from 'react-test-renderer';
+
+afterEach(cleanup);
+
+test('Disply renders correctly', () => {
+  render(<Display />);
+});
+
+describe('<Display />', () => {
+  it('should match snapshot', () => {
+    const tree = renderer.create(<Display />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+test('Gate defaults to unlocked and open', () => {
+  const {getByText} = render(<Display />);
+  getByText(/unlocked/i);
+  getByText(/open/i);
+});
